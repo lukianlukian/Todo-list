3859f72692e5829a5587d8701b1eb7260cfbe27e
Lukian <lukian55d@gmail.com>
2026-03-25 14:26:45 +0000

    Remove __pycache__ directories

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (
+from todolist.views import (
     TaskListView,
     TagListView,
     TagCreateView,
@@ -15,12 +15,14 @@ app_name = "todolist"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+
     path("tags/", TagListView.as_view(), name="tags"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
-    path("task/create/", TaskCreateView.as_view(), name="task-create"),
-    path("task/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tasks/<int:pk>/toggle/", ToggleTaskStatusView.as_view(), name="task-toggle"),
 ]
